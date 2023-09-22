4db36a83980f2b7e94f79ddb0903629873a11ae9
mohammodjuwelis <mohammodjuwelislam@gmail.com>
2023-09-22 13:00:30 +0600

    first commit

---
+++ map.js
@@ -0,0 +1,39 @@
+const numbers = [4,5,6,7,8,9,10];
+
+/* const doubleNumbers = [];
+for(number of numbers) {
+    const doubleNumber = number*number;
+    doubleNumbers.push(doubleNumber);
+}
+console.log(doubleNumbers) */
+
+
+function doubleIt (num) {
+    return num*2
+}
+const result = numbers.map(doubleIt);
+console.log(result);
+
+
+
+const double2 = num => num*num;
+const result2 = numbers.map(double2);
+console.log(result2);
+
+
+const output1 = numbers.map(n => n*n*n);
+console.log(output1);
+
+const halves = numbers.map(n => n/2);
+console.log(halves);
+
+
+const friends = ['Tom', 'John', 'Micheal', 'Oliver'];
+const lengths = friends.map(f => f.length);
+console.log(lengths);
+
+const firstCharacter = friends.map(friend => friend[0]);
+console.log(firstCharacter);
+
+
+
